59b6aa28b9e0bcda2326e82d7665690b729e95a1
brookeconnelly05 <bac6128@psu.edu>
2025-02-08 22:18:21 -0500

    finished

--- src/my-card.js
+++ src/my-card.js
@@ -14,7 +14,11 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
-    this.title = "My card";
+    this.title = 'Free Hug';
+    this.image = 'https://i.etsystatic.com/18796893/r/il/f8534f/4724835275/il_fullxfull.4724835275_kwbm.jpg';
+    this.description = 'A hug for a rough day';
+    this.affirmationsLink = 'https://www.thegoodtrade.com/features/positive-affirmations-morning-routine/';
+    this.detailsLink = 'https://hax.psu.edu';
   }
 
   static get styles() {
@@ -22,16 +26,85 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+
+.bkg {
+background-color:#ccb7e5;
+width: 350px;
+}
+
+img
+{
+    padding:0px 5px 0px 5px;
+  width:340px;
+  height:250px;
+}
+h1{
+  text-align:center;
+  color:#30106b;
+}
+p{
+  margin: 0px;
+  font-size:20px;
+  text-align:center;
+  color:#30106b;
+}
+
+.positive{
+  margin: 15px 4px 10px 50px;
+  font-size:23px;
+  color:#30106b;
+  width:250px;
+  height:50px;
+  background-color:#e6d1f2;
+  border-color:#30106b;
+  border-radius:40px;
+  
+}
+
+button{
+  margin: 15px 10px 20px 140px;
+  font-size:15px;
+  color:#30106b;
+  width:60px;
+  height:30px;
+  background-color:#e6d1f2;
+  border-color:#30106b;
+  border-radius:20px;
+}
+a {
+  text-decoration: none;
+}
     `;
   }
 
   render() {
-    return html`<div>${this.title}</div>`;
+    return html`<div>${this.title}</div>
+    <div class="bkg">
+        <h1>${this.title}</h1>
+        <img src="${this.image}" alt="Image">
+        <p>${this.description}</p>
+        <a href="${this.affirmationsLink}" target="_blank">
+          <button class="positive">Positive Affirmations</button>
+        </a>
+        <a href="${this.detailsLink}" target="_blank">
+          <button class="details">Details</button>
+        </a>
+      </div>
+    
+    
+    
+    
+    `;
+    
   }
 
   static get properties() {
     return {
       title: { type: String },
+      image: { type: String },
+      description: { type: String },
+      affirmationsLink: { type: String },
+      detailsLink: { type: String },
     };
   }
 }
