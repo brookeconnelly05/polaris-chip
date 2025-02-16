8b27ff561d08a102f4fec47d62b3bbcfca17fa05
brookeconnelly05 <bac6128@psu.edu>
2025-02-16 12:35:27 -0500

    modified:   index.html 	modified:   src/my-card.js 	modified:   index.html

--- src/my-card.js
+++ src/my-card.js
@@ -14,11 +14,13 @@ export class MyCard extends LitElement {
 
   constructor() {
     super();
+    this.fancy = false;
     this.title = 'Free Hug';
     this.image = 'https://i.etsystatic.com/18796893/r/il/f8534f/4724835275/il_fullxfull.4724835275_kwbm.jpg';
     this.description = 'A hug for a rough day';
     this.affirmationsLink = 'https://www.thegoodtrade.com/features/positive-affirmations-morning-routine/';
     this.detailsLink = 'https://hax.psu.edu';
+    this.info = "The teddy bear loves you!"
   }
 
   static get styles() {
@@ -26,68 +28,99 @@ export class MyCard extends LitElement {
       :host {
         display: block;
       }
+      
+      :host([fancy]) .bkg {
+        background-color: pink;
+        color: green;
 
-.bkg {
-background-color:#ccb7e5;
-width: 350px;
-}
-
-img
-{
-    padding:0px 5px 0px 5px;
-  width:340px;
-  height:250px;
-}
-h1{
-  text-align:center;
-  color:#30106b;
-}
-p{
-  margin: 0px;
-  font-size:20px;
-  text-align:center;
-  color:#30106b;
 }
 
-.positive{
-  margin: 15px 4px 10px 50px;
-  font-size:23px;
-  color:#30106b;
-  width:250px;
-  height:50px;
-  background-color:#e6d1f2;
-  border-color:#30106b;
-  border-radius:40px;
-  
-}
+      .bkg {
+      background-color:#ccb7e5;
+      width: 350px;
+      }
 
-button{
-  margin: 15px 10px 20px 140px;
-  font-size:15px;
-  color:#30106b;
-  width:60px;
-  height:30px;
-  background-color:#e6d1f2;
-  border-color:#30106b;
-  border-radius:20px;
-}
-a {
-  text-decoration: none;
-}
+      img
+      {
+          padding:0px 5px 0px 5px;
+        width:340px;
+        height:250px;
+      }
+      h1{
+        text-align:center;
+        color:#30106b;
+      }
+      p{
+        margin: 0px;
+        font-size:20px;
+        text-align:center;
+        color:#30106b;
+      }
+
+      .positive{
+        margin: 15px 4px 10px 50px;
+        font-size:23px;
+        color:#30106b;
+        width:250px;
+        height:50px;
+        background-color:#e6d1f2;
+        border-color:#30106b;
+        border-radius:40px;
+        
+      }
+
+      button{
+        margin: 15px 10px 20px 140px;
+        font-size:15px;
+        color:#30106b;
+        width:60px;
+        height:30px;
+        background-color:#e6d1f2;
+        border-color:#30106b;
+        border-radius:20px;
+      }
+      a {
+        text-decoration: none;
+      }
     `;
+  }
+  // put this anywhere on the MyCard class; just above render() is probably good
+  openChanged(e) {
+    console.log(e.newState);
+    if (e.newState === "open") {
+      this.fancy = true;
+    }
+    else {
+      this.fancy = false;
+    }
   }
 
   render() {
-    return html`<div>${this.title}</div>
+    
+    return html`
+    <div>${this.title}</div>
+  
     <div class="bkg">
         <h1>${this.title}</h1>
         <img src="${this.image}" alt="Image">
         <p>${this.description}</p>
+        <slot></slot>
+        <!-- put this in your render method where you had details -->
+        <details ?open="${this.fancy}" @toggle="${this.openChanged}">
+        <summary>Description</summary>
+          <div>
+         <slot>${this.info}</slot>
+         <a href="${this.detailsLink}" target="_blank">
+          <button class="details">Details</button>
+          </a>
+          </div>
+          
+          </details>
+          
         <a href="${this.affirmationsLink}" target="_blank">
           <button class="positive">Positive Affirmations</button>
-        </a>
-        <a href="${this.detailsLink}" target="_blank">
-          <button class="details">Details</button>
+        
+        
         </a>
       </div>
     
@@ -100,11 +133,13 @@ a {
 
   static get properties() {
     return {
+      fancy: { type: Boolean, reflect: true },
       title: { type: String },
       image: { type: String },
       description: { type: String },
       affirmationsLink: { type: String },
       detailsLink: { type: String },
+      info: {type: String},
     };
   }
 }
